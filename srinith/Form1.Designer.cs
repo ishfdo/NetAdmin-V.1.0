411d9b0a7b4c888534cf4671de2b4dbeef30408f
ishan fernando <isfdo6@gmail.com>
2016-08-05 12:39:37 +0530

    Add existing filles

---
+++ srinith/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace srinith
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardwareInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkSumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subnetCalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.networkTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.remoteConnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.screenshotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.screenshotToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(618, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hardwareInfoToolStripMenuItem,
+            this.uToolStripMenuItem,
+            this.checkSumToolStripMenuItem,
+            this.subnetCalToolStripMenuItem,
+            this.networkTestToolStripMenuItem,
+            this.remoteConnectToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // hardwareInfoToolStripMenuItem
+            // 
+            this.hardwareInfoToolStripMenuItem.Name = "hardwareInfoToolStripMenuItem";
+            this.hardwareInfoToolStripMenuItem.Size = new System.Drawing.Size(188, 24);
+            this.hardwareInfoToolStripMenuItem.Text = "Hardware Info";
+            this.hardwareInfoToolStripMenuItem.Click += new System.EventHandler(this.hardwareInfoToolStripMenuItem_Click);
+            // 
+            // uToolStripMenuItem
+            // 
+            this.uToolStripMenuItem.Name = "uToolStripMenuItem";
+            this.uToolStripMenuItem.Size = new System.Drawing.Size(188, 24);
+            this.uToolStripMenuItem.Text = "Usage Monitor";
+            // 
+            // checkSumToolStripMenuItem
+            // 
+            this.checkSumToolStripMenuItem.Name = "checkSumToolStripMenuItem";
+            this.checkSumToolStripMenuItem.Size = new System.Drawing.Size(188, 24);
+            this.checkSumToolStripMenuItem.Text = "Check Sum";
+            // 
+            // subnetCalToolStripMenuItem
+            // 
+            this.subnetCalToolStripMenuItem.Name = "subnetCalToolStripMenuItem";
+            this.subnetCalToolStripMenuItem.Size = new System.Drawing.Size(188, 24);
+            this.subnetCalToolStripMenuItem.Text = "Subnet Cal";
+            // 
+            // networkTestToolStripMenuItem
+            // 
+            this.networkTestToolStripMenuItem.Name = "networkTestToolStripMenuItem";
+            this.networkTestToolStripMenuItem.Size = new System.Drawing.Size(188, 24);
+            this.networkTestToolStripMenuItem.Text = "Network Test";
+            // 
+            // remoteConnectToolStripMenuItem
+            // 
+            this.remoteConnectToolStripMenuItem.Name = "remoteConnectToolStripMenuItem";
+            this.remoteConnectToolStripMenuItem.Size = new System.Drawing.Size(188, 24);
+            this.remoteConnectToolStripMenuItem.Text = "Remote Connect";
+            // 
+            // screenshotToolStripMenuItem
+            // 
+            this.screenshotToolStripMenuItem.Name = "screenshotToolStripMenuItem";
+            this.screenshotToolStripMenuItem.Size = new System.Drawing.Size(93, 24);
+            this.screenshotToolStripMenuItem.Text = "Screenshot";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.aboutToolStripMenuItem.Text = "About us";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(58, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Usage Monitor";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(58, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Check Sum";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(338, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(201, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Subnet Cal";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(338, 140);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(201, 43);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Network Test";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(338, 221);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(201, 43);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Remote Test";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(500, 414);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(91, 33);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Exit";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(58, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hardware Info";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Location = new System.Drawing.Point(109, 303);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(376, 88);
+            this.panel1.TabIndex = 8;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(24, 26);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(92, 37);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Shutdown";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(142, 26);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(92, 37);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "Restart";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(259, 26);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(92, 37);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "Sleep";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(618, 478);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "NetAdmin V.1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardwareInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkSumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subnetCalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem networkTestToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem remoteConnectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem screenshotToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
