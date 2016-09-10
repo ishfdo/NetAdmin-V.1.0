9cf9406ebd54fb5668d3e5b11ef5756c8a9364ff
Nishen Peiris <nishenkpeiris@gmail.com>
2016-09-10 17:26:10 +0530

    Implement GetBroadcastAddress and GetSubnetID functions

--- srinith/SubnetCal.cs
+++ srinith/SubnetCal.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +27,42 @@ namespace srinith
         private void btnCalculate_Click(object sender, EventArgs e)
         {
 
+        }
+
+        /* ---------- Required functions ---------- */
+
+        // This method returns the broadcast IP address when an IP address and it's subnet mask is given
+        public IPAddress GetBroadcastAddress(IPAddress address, IPAddress subnetMask)
+        {
+            byte[] ipAdressBytes = address.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAdressBytes.Length != subnetMaskBytes.Length)
+                MessageBox.Show("Lengths of IP address and subnet mask do not match.");
+
+            byte[] broadcastAddress = new byte[ipAdressBytes.Length];
+            for (int i = 0; i < broadcastAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(ipAdressBytes[i] | (subnetMaskBytes[i] ^ 255));
+            }
+            return new IPAddress(broadcastAddress);
+        }
+
+        // This function returns the subnet ID when an IP address and it's subnet mask is given
+        public IPAddress GetSubnetID(IPAddress address, IPAddress subnetMask)
+        {
+            byte[] ipAdressBytes = address.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAdressBytes.Length != subnetMaskBytes.Length)
+                throw new ArgumentException("Lengths of IP address and subnet mask do not match.");
+
+            byte[] broadcastAddress = new byte[ipAdressBytes.Length];
+            for (int i = 0; i < broadcastAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(ipAdressBytes[i] & (subnetMaskBytes[i]));
+            }
+            return new IPAddress(broadcastAddress);
         }
     }
 }
