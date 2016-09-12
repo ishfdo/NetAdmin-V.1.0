4bc321b43edb7728851fc22323ee033d32c34a88
Nishen Peiris <nishenkpeiris@gmail.com>
2016-09-12 11:22:31 +0530

    Add the UI elements on the form

--- srinith/CheckSum.Designer.cs
+++ srinith/CheckSum.Designer.cs
@@ -28,21 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.txtChecksum = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.lblMatch = new System.Windows.Forms.Label();
+            this.lblMismatch = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Checksum";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Title = "Browse for file";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(221, 130);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(100, 20);
+            this.txtFileName.TabIndex = 2;
+            // 
+            // txtChecksum
+            // 
+            this.txtChecksum.Location = new System.Drawing.Point(221, 158);
+            this.txtChecksum.Name = "txtChecksum";
+            this.txtChecksum.Size = new System.Drawing.Size(100, 20);
+            this.txtChecksum.TabIndex = 3;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(327, 128);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 4;
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Location = new System.Drawing.Point(246, 184);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(75, 23);
+            this.btnVerify.TabIndex = 5;
+            this.btnVerify.Text = "Verify";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            // 
+            // lblMatch
+            // 
+            this.lblMatch.AutoSize = true;
+            this.lblMatch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatch.Location = new System.Drawing.Point(142, 230);
+            this.lblMatch.Name = "lblMatch";
+            this.lblMatch.Size = new System.Drawing.Size(0, 13);
+            this.lblMatch.TabIndex = 6;
+            // 
+            // lblMismatch
+            // 
+            this.lblMismatch.AutoSize = true;
+            this.lblMismatch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMismatch.ForeColor = System.Drawing.Color.Red;
+            this.lblMismatch.Location = new System.Drawing.Point(142, 230);
+            this.lblMismatch.Name = "lblMismatch";
+            this.lblMismatch.Size = new System.Drawing.Size(0, 13);
+            this.lblMismatch.TabIndex = 7;
             // 
             // CheckSum
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.lblMismatch);
+            this.Controls.Add(this.lblMatch);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txtChecksum);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "CheckSum";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Check Sum";
             this.Load += new System.EventHandler(this.CheckSum_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.TextBox txtChecksum;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.Label lblMatch;
+        private System.Windows.Forms.Label lblMismatch;
     }
 }
--- srinith/CheckSum.cs
+++ srinith/CheckSum.cs
@@ -19,7 +19,22 @@ namespace srinith
 
         private void CheckSum_Load(object sender, EventArgs e)
         {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
+            // Configure UI elements
+            btnOpenFile.Text = "Upload File";
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            // Configure UI elements
+            btnOpenFile.Text = "Change File";
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
         }
     }
 }
